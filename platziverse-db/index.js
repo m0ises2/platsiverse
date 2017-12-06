e16e4bfe345d1d84c73dfe7a759faab941fc1e16
Moises Alvarado <moisesalvarado84@gmail.com>
2017-12-05 18:48:29 -0600

    Pruebas con AVA

--- platziverse-db/index.js
+++ platziverse-db/index.js
@@ -3,8 +3,21 @@
 const setupAgentModel = require('./models/agent')
 const setupMetricModel = require('./models/metric')
 const setupDatabase = require('./lib/db')
+const defaults = require('defaults')
 
 module.exports = async function (config) {
+  config = defaults(config, {
+    dialect: 'sqlite',
+    pool: {
+      max: 10,
+      min: 0,
+      idel: 10000
+    },
+    query: {
+      raw: true
+    }
+  })
+
   // Modelos:
   const sequelize = setupDatabase(config)
   const AgentModel = setupAgentModel(config)
@@ -17,6 +30,10 @@ module.exports = async function (config) {
   MetricModel.belongsTo(AgentModel)
 
   await sequelize.authenticate()
+
+  if (config.setup) {
+    await sequelize.sync({ force: true })
+  }
 
   // Instancias de los modelos ya vinculados con sus respectivas librerías y relacionados con los demas modelos:
   const Agent = {}
---
+++ platziverse-db/setup.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const debug = require('debug')('platziverse:db:setup')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+const db = require('./')
+
+const prompt = inquirer.createPromptModule()
+
+async function setup () {
+  const anwser = await prompt([
+    {
+      type: 'confirm',
+      name: 'setup',
+      message: 'This will destroy the database, are you sure?'
+    }
+  ])
+
+  if (!anwser.setup) return console.log('Nothing happens :)')
+
+  const config = {
+    database: process.env.DB_NAME || 'platziverse',
+    username: process.env.DB_USER || 'platzi',
+    password: process.env.DB_PASS || 'platzi',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
+    logging: s => debug(s),
+    setup: true
+  }
+
+  await db(config).catch(handleFatalError)
+
+  console.log('Success!')
+  process.exit(0)
+}
+
+function handleFatalError (err) {
+  console.error(`${chalk.red('[Fatal error]')} ${err.message}`)
+  console.error(err.stack)
+
+  process.exit(1)
+}
+
+setup()
